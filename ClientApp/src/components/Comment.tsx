9052c0c0a964db90cddfefcce18b1a75339663b5
mxstrong <mantas.ptakauskas@gmail.com>
2020-08-31 18:49:16 +0300

    Refactor comments into new component

---
+++ ClientApp/src/components/Comment.tsx
@@ -0,0 +1,248 @@
+import { IComment } from "../helpers/types";
+import { useDispatch, useSelector } from "react-redux";
+import { AppState } from "../reducers";
+import React, { useState } from "react";
+import {
+  EDIT_COMMENT_URL,
+  DELETE_COMMENT_URL,
+  ADD_COMMENT_URL,
+} from "../constants/urls";
+import { fetchComments } from "../actions";
+import {
+  Card,
+  TextField,
+  Button,
+  CardHeader,
+  CardContent,
+  CardActionArea,
+  makeStyles,
+  Theme,
+} from "@material-ui/core";
+import { role } from "../constants/roles";
+
+interface ICommentProps {
+  comment: IComment;
+  parentId: string | null;
+}
+
+export interface IState {
+  parentId: string | null;
+  text: string;
+  postId: string;
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  comment: {
+    margin: theme.spacing(2),
+    width: "300px",
+  },
+  commentWrapper: {
+    marginLeft: theme.spacing(2),
+  },
+  header: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function Comment(props: ICommentProps) {
+  const { comment, parentId } = props;
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const userProfile = useSelector((state: AppState) => state.auth.userProfile);
+  const userToken = useSelector((state: AppState) => state.auth.user);
+  const post = useSelector((state: AppState) => state.posts.currentPost);
+
+  let [commentToEdit, setCommentToEdit] = useState({
+    commentId: "",
+    text: "",
+    postId: post ? post.postId : "",
+    userId: userProfile.userId,
+  });
+
+  let [newComment, setNewComment] = useState<IState>({
+    parentId: null,
+    text: "",
+    postId: post ? post.postId : "",
+  });
+
+  if (comment.parentId !== null && comment.parentId !== parentId) {
+    return <React.Fragment></React.Fragment>;
+  }
+
+  async function handleEdit() {
+    const response = await fetch(
+      EDIT_COMMENT_URL + "/" + commentToEdit.commentId,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: "Bearer " + userToken,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(commentToEdit),
+      }
+    );
+    if (response.ok) {
+      setCommentToEdit({
+        commentId: "",
+        text: "",
+        postId: post ? post.postId : "",
+        userId: userProfile.userId,
+      });
+      dispatch(fetchComments(post ? post.postId : ""));
+    }
+  }
+
+  async function handleDelete(commentId: string) {
+    const response = await fetch(DELETE_COMMENT_URL + "/" + commentId, {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + userToken,
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      dispatch(fetchComments(post ? post.postId : ""));
+    }
+  }
+
+  async function handleClick() {
+    const response = await fetch(ADD_COMMENT_URL, {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + userToken,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newComment),
+    });
+    if (response.ok) {
+      setNewComment({
+        parentId: null,
+        text: "",
+        postId: post ? post.postId : "",
+      });
+      dispatch(fetchComments(post ? post.postId : ""));
+    }
+  }
+
+  return (
+    <div className={classes.commentWrapper}>
+      <Card key={comment.commentId} className={classes.comment}>
+        {comment.commentId == commentToEdit.commentId ? (
+          <React.Fragment>
+            <TextField
+              multiline
+              placeholder="Write updated comment's text here"
+              value={commentToEdit.text}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                setCommentToEdit({
+                  ...commentToEdit,
+                  text: event.target.value,
+                })
+              }
+              variant="outlined"
+            />
+            <Button color="primary" onClick={handleEdit}>
+              Update Comment
+            </Button>
+            <Button
+              color="secondary"
+              onClick={() =>
+                setCommentToEdit({
+                  commentId: "",
+                  text: "",
+                  postId: post ? post.postId : "",
+                  userId: userProfile.userId,
+                })
+              }
+            >
+              Cancel
+            </Button>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <CardHeader
+              className={classes.header}
+              action={
+                userProfile.role === role.admin ||
+                userProfile.userId === comment.userId ? (
+                  <React.Fragment>
+                    <Button
+                      onClick={() =>
+                        setCommentToEdit({
+                          ...commentToEdit,
+                          commentId: comment.commentId,
+                          text: comment.text,
+                        })
+                      }
+                    >
+                      Edit
+                    </Button>
+                    <Button
+                      color="secondary"
+                      onClick={() => handleDelete(comment.commentId)}
+                    >
+                      Delete
+                    </Button>
+                  </React.Fragment>
+                ) : (
+                  ""
+                )
+              }
+              title={comment.author}
+            />
+            <CardContent>{comment.text}</CardContent>
+            <CardActionArea>
+              <Button
+                color="primary"
+                onClick={() => {
+                  if (newComment.parentId !== comment.commentId) {
+                    setNewComment({
+                      ...newComment,
+                      parentId: comment.commentId,
+                    });
+                  } else {
+                    setNewComment({
+                      ...newComment,
+                      parentId: null,
+                    });
+                  }
+                }}
+              >
+                Reply
+              </Button>
+            </CardActionArea>
+            {newComment.parentId == comment.commentId ? (
+              <React.Fragment>
+                <TextField
+                  variant="outlined"
+                  multiline
+                  placeholder="Write comment's text here"
+                  value={newComment.text}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    setNewComment({
+                      ...newComment,
+                      text: event.target.value,
+                    })
+                  }
+                />
+                <Button color="primary" onClick={handleClick}>
+                  Comment
+                </Button>
+              </React.Fragment>
+            ) : (
+              ""
+            )}
+          </React.Fragment>
+        )}
+      </Card>
+      {comment.children
+        ? comment.children.map((child) => (
+            <Comment comment={child} parentId={comment.commentId} />
+          ))
+        : ""}
+    </div>
+  );
+}
--- ClientApp/src/components/ViewPost.tsx
+++ ClientApp/src/components/ViewPost.tsx
@@ -17,25 +17,11 @@ import { Redirect } from "react-router-dom";
 import { fetchCurrentPost, fetchComments } from "../actions";
 import { IComment } from "../helpers/types";
 import { role } from "../constants/roles";
-import {
-  ADD_COMMENT_URL,
-  EDIT_COMMENT_URL,
-  DELETE_COMMENT_URL,
-} from "../constants/urls";
+import { ADD_COMMENT_URL } from "../constants/urls";
+
+import Comment, { IState } from "./Comment";
 
 const useStyles = makeStyles((theme: Theme) => ({
-  comment: {
-    margin: theme.spacing(2),
-    width: "300px",
-  },
-  commentWrapper: {
-    marginLeft: theme.spacing(2),
-  },
-  header: {
-    "& > *": {
-      margin: theme.spacing(1),
-    },
-  },
   post: {
     display: "flex",
     flexDirection: "column",
@@ -49,12 +35,6 @@ interface IProps {
   postId: string | null;
 }
 
-interface IState {
-  parentId: string | null;
-  text: string;
-  postId: string;
-}
-
 export default function ViewPost(props: IProps) {
   const classes = useStyles();
   const dispatch = useDispatch();
@@ -66,7 +46,6 @@ export default function ViewPost(props: IProps) {
     }
   }, []);
   const userToken = useSelector((state: AppState) => state.auth.user);
-  const userProfile = useSelector((state: AppState) => state.auth.userProfile);
   const post = useSelector((state: AppState) => state.posts.currentPost);
   const comments = useSelector((state: AppState) => state.comments.comments);
 
@@ -76,139 +55,8 @@ export default function ViewPost(props: IProps) {
     postId: post ? post.postId : "",
   });
 
-  let [commentToEdit, setCommentToEdit] = useState({
-    commentId: "",
-    text: "",
-    postId: post ? post.postId : "",
-    userId: userProfile.userId,
-  });
-
   if (!post) {
     return <Redirect to="/posts" />;
-  }
-
-  function renderComment(comment: IComment, parentId: string | null = null) {
-    if (comment.parentId !== null && comment.parentId !== parentId) {
-      return;
-    }
-    return (
-      <div className={classes.commentWrapper}>
-        <Card key={comment.commentId} className={classes.comment}>
-          {comment.commentId == commentToEdit.commentId ? (
-            <React.Fragment>
-              <TextField
-                multiline
-                placeholder="Write updated comment's text here"
-                value={commentToEdit.text}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                  setCommentToEdit({
-                    ...commentToEdit,
-                    text: event.target.value,
-                  })
-                }
-                variant="outlined"
-              />
-              <Button color="primary" onClick={handleEdit}>
-                Update Comment
-              </Button>
-              <Button
-                color="secondary"
-                onClick={() =>
-                  setCommentToEdit({
-                    commentId: "",
-                    text: "",
-                    postId: post ? post.postId : "",
-                    userId: userProfile.userId,
-                  })
-                }
-              >
-                Cancel
-              </Button>
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              <CardHeader
-                className={classes.header}
-                action={
-                  userProfile.role === role.admin ||
-                  userProfile.userId === comment.userId ? (
-                    <React.Fragment>
-                      <Button
-                        onClick={() =>
-                          setCommentToEdit({
-                            ...commentToEdit,
-                            commentId: comment.commentId,
-                            text: comment.text,
-                          })
-                        }
-                      >
-                        Edit
-                      </Button>
-                      <Button
-                        color="secondary"
-                        onClick={() => handleDelete(comment.commentId)}
-                      >
-                        Delete
-                      </Button>
-                    </React.Fragment>
-                  ) : (
-                    ""
-                  )
-                }
-                title={comment.author}
-              />
-              <CardContent>{comment.text}</CardContent>
-              <CardActionArea>
-                <Button
-                  color="primary"
-                  onClick={() => {
-                    if (newComment.parentId !== comment.commentId) {
-                      setNewComment({
-                        ...newComment,
-                        parentId: comment.commentId,
-                      });
-                    } else {
-                      setNewComment({
-                        ...newComment,
-                        parentId: null,
-                      });
-                    }
-                  }}
-                >
-                  Reply
-                </Button>
-              </CardActionArea>
-              {newComment.parentId == comment.commentId ? (
-                <React.Fragment>
-                  <TextField
-                    variant="outlined"
-                    multiline
-                    placeholder="Write comment's text here"
-                    value={newComment.text}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                      setNewComment({
-                        ...newComment,
-                        text: event.target.value,
-                      })
-                    }
-                  />
-                  <Button color="primary" onClick={handleClick}>
-                    Comment
-                  </Button>
-                </React.Fragment>
-              ) : (
-                ""
-              )}
-            </React.Fragment>
-          )}
-        </Card>
-        {comment.children
-          ? comment.children.map((child) =>
-              renderComment(child, comment.commentId)
-            )
-          : ""}
-      </div>
-    );
   }
 
   async function handleClick() {
@@ -226,42 +74,6 @@ export default function ViewPost(props: IProps) {
         text: "",
         postId: post ? post.postId : "",
       });
-      dispatch(fetchComments(post ? post.postId : ""));
-    }
-  }
-
-  async function handleEdit() {
-    const response = await fetch(
-      EDIT_COMMENT_URL + "/" + commentToEdit.commentId,
-      {
-        method: "PUT",
-        headers: {
-          Authorization: "Bearer " + userToken,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(commentToEdit),
-      }
-    );
-    if (response.ok) {
-      setCommentToEdit({
-        commentId: "",
-        text: "",
-        postId: post ? post.postId : "",
-        userId: userProfile.userId,
-      });
-      dispatch(fetchComments(post ? post.postId : ""));
-    }
-  }
-
-  async function handleDelete(commentId: string) {
-    const response = await fetch(DELETE_COMMENT_URL + "/" + commentId, {
-      method: "DELETE",
-      headers: {
-        Authorization: "Bearer " + userToken,
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
       dispatch(fetchComments(post ? post.postId : ""));
     }
   }
@@ -294,7 +106,9 @@ export default function ViewPost(props: IProps) {
 
       <React.Fragment>
         {comments && comments.length > 0
-          ? comments.map((comment: IComment) => renderComment(comment))
+          ? comments.map((comment: IComment) => (
+              <Comment comment={comment} parentId={null} />
+            ))
           : "No comments yet..."}
       </React.Fragment>
     </Paper>
