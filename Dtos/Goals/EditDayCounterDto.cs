93d467683c3af157b499f897cf13e94e41b790bb
mxstrong <mantas.ptakauskas@gmail.com>
2020-11-05 12:53:57 +0200

    Fix bugs with editing dayCounters

--- Dtos/Goals/EditDayCounterDto.cs
+++ Dtos/Goals/EditDayCounterDto.cs
@@ -5,6 +5,6 @@ namespace Mxstrong.Dtos
   public class EditDayCounterDto : EditGoalDto
   {
     public DateTime StartingDate { get; set; }
-    public int DayGoal { get; set; }
+    public string DayGoal { get; set; }
   }
 }
